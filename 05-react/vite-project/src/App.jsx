fc16deff56683ace7622e39b6a220846de145c03
Kaushal Prajapati <kaushalraj059@gmail.com>
2026-05-03 23:52:18 +0530

    feat: initialize React project, understand file structure, and update App.jsx and App.css

---
+++ 05-react/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div id="centre">
+            <h1>Hello World, It's me Kaushal!</h1>
+            <button class="counter">click me!</button>
+        </div>
+    );
+}
+
+export default App;
