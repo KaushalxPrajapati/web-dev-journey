267200961cc83b6e477d01b534b3646e87fd005b
Kaushal Prajapati <kaushalraj059@gmail.com>
2026-05-18 07:12:51 +0530

    fix: update form.html and index.js

--- 03-backend/ExpressJS/03-get-post-req/Backend/index.js
+++ 03-backend/ExpressJS/03-get-post-req/Backend/index.js
@@ -5,16 +5,14 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`App is listening on the PORT: ${port}, Go Check!`);
-});
-
 // Handle the form submission on GET /submit
 app.get('/submit', (req, res) => {
-    let { name } = req.query;
+    // Will show in Frontend:
+    let { name } = req.query; // For GET request, the data is sent in the query string, so we can access it via req.query
     res.send(`Your GET request has been received. Welcome ${name}`);
 
-    console.log('GET REQUEST Received!');
+    // Will show in Backend console log:
+    console.log('GET REQUEST Received From Frontend!');
     console.log(req.query);
 });
 
@@ -24,12 +22,18 @@ app.post('/submit', (req, res) => {
     // let name = req.body.name;
     // let password = req.body.password;
 
-    // Method 2 to extract the data from req.body (destructuring)
+    // Method 2 to extract the data from req.body by destructuring, which is more concise and cleaner:
     // let { name, password } = req.body; // POST data lives in req.body, not req.params or req.query
 
+    // Will show in Frontend:
     let { name } = req.body;
     res.send(`Your POST request has been received. Welcome ${name}`);
 
-    console.log('POST REQUEST Received!');
+    // Will show in Backend console log:
+    console.log('POST REQUEST Received From Frontend!');
     console.log(req.body);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at PORT:${port}, Go Check form.html and open with live server`);
 });
